b7ee9284ba5726aa9aa25b0c2c4af9707b4c5a9d
Quadro77 <40344575+Quadro77@users.noreply.github.com>
2024-05-14 08:22:47 +0930

    Update install_CUDA.sh

--- install_CUDA.sh
+++ install_CUDA.sh
@@ -1,6 +1,9 @@
+# This did not work
 apt  install cmake
 sudo bash
 git clone https://github.com/xmrig/xmrig-cuda.git
 mkdir xmrig-cuda/build && cd xmrig-cuda/build
 apt install nvidia-cuda-toolkit (-y? enable confirm)
 reboot
+sudo apt purge nvidia-cuda-toolkit
+
